2b4fe8f7427f68cbae26cf590ad71552448e65a0
resteve <resteve@zikzakmedia.com>
2012-12-03 11:49:36 +0100

    Header copyright files

--- src/easyimap/easyimap.py
+++ src/easyimap/easyimap.py
@@ -1,5 +1,7 @@
+#This file is part easyimap.
+#The COPYRIGHT file at the top level of this repository contains
+#the full copyright notices and license terms.
 # -*- coding: utf-8 -*-
-
 import imaplib
 import email
 import time
