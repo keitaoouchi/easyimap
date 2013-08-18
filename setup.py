28608d7f5bb0147090058ce824a476e4754107d6
keitaoouchi <keita.oouchi@gmail.com>
2013-08-19 00:07:00 +0900

    version++

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     author_email='keita.oouchi@gmail.com',
     url='https://github.com/keitaoouchi/easyimap',
     name='easyimap',
-    version='0.3.4',
+    version='0.3.5',
     package_dir={"": "src"},
     packages=['easyimap'],
     license='BSD License',
