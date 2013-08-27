ed4e30c0e7c2b5aec6cfdf821eba0bab2cd232c3
keitaoouchi <keita.oouchi@gmail.com>
2013-08-27 14:06:09 +0900

    Convert single line comments to docstrings

--- src/easyimap/easyimap.py
+++ src/easyimap/easyimap.py
@@ -1,7 +1,11 @@
-#This file is part easyimap.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
 # -*- coding: utf-8 -*-
+
+"""
+This file is part easyimap.
+The COPYRIGHT file at the top level of this repository contains
+the full copyright notices and license terms.
+"""
+
 import imaplib
 import email
 import time
